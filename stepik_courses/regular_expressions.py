095bb6dc0ce32f0124b75ad01ab9b9fffaf8ca00
chertkov-nikita <nikita.chertkov@yandex.ru>
2023-01-09 21:31:07 +0300

    added realization of third to fifth tasks

--- stepik_courses/regular_expressions.py
+++ stepik_courses/regular_expressions.py
@@ -14,15 +14,42 @@ def first_task(string):
 # 2. Выведите строки, содержащие "cat" в качестве слова.
 def second_task(string):
     pattern = r"\bcat\b"
-    if len(re.findall(pattern, string)):
+    if re.search(pattern, string) is not None:
+        print(string)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 3. Выведите строки, содержащие две буквы "z", между которыми ровно три символа.
+def third_task(string):
+    pattern = r"z.{3}z"
+    if re.search(pattern, string) is not None:
+        print(string)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 4. Выведите строки, содержащие обратный слеш "\".
+def fourth_task(string):
+    pattern = r"\\"
+    if re.search(pattern, string) is not None:
+        print(string)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 5. Выведите строки, содержащие слово, состоящее из двух одинаковых частей (тандемный повтор).
+def fifth_task(string):
+    pattern = r"\b(.+?)\1\b"
+    if re.search(pattern, string) is not None:
         print(string)
 
 
 def main():
     for line in sys.stdin:
         line = line.rstrip()
-        # first_task(line)
+        first_task(line)
         second_task(line)
+        third_task(line)
+        fourth_task(line)
+        fifth_task(line)
 
 
 if __name__ == '__main__':
