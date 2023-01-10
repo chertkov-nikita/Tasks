688ef53b392ae301b686b9d24ad3ac0e3a9eb124
chertkov-nikita <nikita.chertkov@yandex.ru>
2023-01-10 20:35:29 +0300

    added realization of sixth to ninth tasks

--- stepik_courses/regular_expressions.py
+++ stepik_courses/regular_expressions.py
@@ -3,7 +3,6 @@ import sys
 
 
 # 1. Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
-
 def first_task(string):
     pattern = r"cat"
     if len(re.findall(pattern, string)) >= 2:
@@ -42,6 +41,32 @@ def fifth_task(string):
         print(string)
 
 
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 6. В каждой строке замените все вхождения подстроки "human" на подстроку "computer" и выведите полученные строки.
+def sixth_task(string):
+    pattern = r"human"
+    print(string.replace(pattern, "computer"))
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 7. В каждой строке замените первое вхождение слова,
+# состоящего только из латинских букв "a" (регистр не важен), на слово "argh".
+def seventh_task(string):
+    print(re.sub(r"\ba+\b", "argh", string, count=1, flags=re.IGNORECASE))
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 8. В каждой строке поменяйте местами две первых буквы в каждом слове, состоящем хотя бы из двух букв.
+def eighth_task(string):
+    print(re.sub(r"\b(\w)(\w)(\w*)\b", r"\2\1\3", string))
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+# 9. В каждой строке замените все вхождения нескольких одинаковых букв на одну букву.
+def ninth_task(string):
+    print(re.sub(r"(\w)\1+", r"\1", string))
+
+
 def main():
     for line in sys.stdin:
         line = line.rstrip()
@@ -50,6 +75,10 @@ def main():
         third_task(line)
         fourth_task(line)
         fifth_task(line)
+        sixth_task(line)
+        seventh_task(line)
+        eighth_task(line)
+        ninth_task(line)
 
 
 if __name__ == '__main__':
