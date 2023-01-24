2328db6a8edab1312cecb9f487cbe5a480731494
chertkov-nikita <nikita.chertkov@yandex.ru>
2023-01-24 14:58:39 +0300

    added realization second tasks A function was made that processes the classes written in JSON format and displays the number of heirs for each class.

--- stepik_courses/text_file_formats.py
+++ stepik_courses/text_file_formats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from json import loads
 
 
 # 1. Вам необходимо узнать тип преступления, которое было зафиксировано максимальное
@@ -11,8 +12,50 @@ def first_task(year):
     return data['Primary Type'].value_counts().to_frame().idxmax()
 
 
+# /////////////////////////////////////////////////////////////////////////////////////////
+# 2. Вам дано описание наследования классов в формате JSON.
+# Описание представляет из себя массив JSON-объектов, которые соответствуют классам.
+# У каждого JSON-объекта есть поле name, которое содержит имя класса, и поле parents,
+# которое содержит список имен прямых предков.
+# Для каждого класса вычислите предком скольких классов он является и выведите эту информацию.
+def get_parents_list(class_name, data):
+    for obj in data:
+        if obj['name'] == class_name:
+            return obj['parents']
+    return []
+
+
+def check_in_parents_list(from_class_name, to_class_name, data):
+    for parent in get_parents_list(from_class_name, data):
+        if parent == to_class_name:
+            return True
+        else:
+            ancestor = check_in_parents_list(parent, to_class_name, data)
+            if ancestor:
+                return ancestor
+    return False
+
+
+def second_task():
+    data = loads(input())
+    result_dict = {}
+    for parent in data:
+        for child in data:
+            if child['name'] not in result_dict:
+                result_dict[child['name']] = 1
+            if parent['name'] != child['name'] and check_in_parents_list(parent['name'],
+                                                                         child['name'],
+                                                                         data):
+                result_dict[child['name']] += 1
+
+    result_lst = [{"name": name, "value": result_dict[name]} for name in result_dict]
+    for el in sorted(result_lst, key=lambda obj: obj['name']):
+        print(f"{el['name']} : {el['value']}")
+
+
 def main():
     print(first_task('2015'))
+    second_task()
 
 
 if __name__ == '__main__':
